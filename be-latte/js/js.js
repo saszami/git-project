49b3b0fdd2af35621f7f2bcea24f93043a2ecec6
saszami <saszami@mail.ru>
2018-07-20 00:23:23 +0300

    responsive design for be-latte + add BestPapa, my coursetask on Web Developer 9.0 by Академия верстки Артема Исламова

--- be-latte/js/js.js
+++ be-latte/js/js.js
@@ -1,24 +1,36 @@
 
+  $(document).ready(function(){
+    /*Модальное окно с формой связи*/
+    $('.popup-btn').on('click', function(event) {
+      event.preventDefault();
+      $('#popup-menu').fadeIn()
+      });
+    $('.popup-close').on('click', function(event) {
+      event.preventDefault();
+      $('#popup-menu').fadeOut()
+    });
+  });
+
+
+// slider
+
     $(document).ready(function(){
       $('.feedback-slider').slick({
         dots: true,
-         arrows: false
+        arrows: false
       });
     });
 
+// popup
 
-    // initMap() - функция инициализации карты
-    function initMap() {
-    	// Координаты центра на карте. Широта: 56.2928515, Долгота: 43.7866641
-    	var centerLatLng = new google.maps.LatLng(29.629270, -96.060123);
-    	// Обязательные опции с которыми будет проинициализированна карта
-    	var mapOptions = {
-    		center: centerLatLng, // Координаты центра мы берем из переменной centerLatLng
-    		zoom: 15,            // Зум по умолчанию. Возможные значения от 0 до 21
-    		disableDefaultUI: true // убрала кнопки навигации
-    	};
-    	// Создаем карту внутри элемента #map
-    	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-    }
-    // Ждем полной загрузки страницы, после этого запускаем initMap()
-    google.maps.event.addDomListener(window, "load", initMap);
+// google map
+
+    var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: 29.629270, lng: -96.060123},
+          zoom: 15,
+          disableDefaultUI: true
+        });
+      };
+
